a5ddf66424a753757c0f094162de83e3fadf524b
Mattia Basaglia <glax@dragon.best>
2021-10-21 12:06:08 +0200

    Comprehensive schema diff

--- tools/schema-info.py
+++ tools/schema-info.py
@@ -41,6 +41,11 @@ class SchemaLocation:
                 for aggregate in schema_object[aggregate_type]:
                     self.inspect_object(aggregate, object_summary)
 
+        if schema_object.get("type", None) != "object" and "oneOf" in schema_object:
+            for enum in schema_object["oneOf"]:
+                if "const" in enum:
+                    object_summary.properties.add(enum["const"])
+
         if "if" in schema_object:
             for condition in ["then", "else"]:
                 if condition in schema_object:
@@ -83,9 +88,11 @@ def cmd_summary(ns):
         print(" * " + prop)
 
 
-def diff_sets(vals1, vals2):
+def diff_sets(vals1, vals2, show_same):
     for item in vals1|vals2:
         if item in vals1 and item in vals2:
+            if not show_same:
+                continue
             op = " "
         elif item in vals1:
             op = "-"
@@ -100,9 +107,110 @@ def cmd_diff(ns):
     schema_2 = SchemaLocationPath(ns.root_2)
     summary_2 = schema_2.get_summary(schema_2.fetch(ns.object_ref_2))
     print("properties:")
-    diff_sets(summary_1.properties, summary_2.properties)
+    diff_sets(summary_1.properties, summary_2.properties, True)
     print("required:")
-    diff_sets(summary_1.required, summary_2.required)
+    diff_sets(summary_1.required, summary_2.required, True)
+
+
+def cmd_compare(ns):
+    mappings = [
+        # Community / lottie-docs
+        ("animation", "animation/animation"),
+        ("effects/angle", "effects/effect-value-angle"),
+        ("effects/checkBox", "effects/effect-value-checkbox"),
+        ("effects/color", "effects/effect-value-color"),
+        #("effects/customValue", "effects/effect-value-"),
+        ("effects/dropDown", "effects/effect-value-drop-down"),
+        ("effects/fill", "effects/fill-effect"),
+        #("effects/group", "effects/"),
+        #("effects/index", "effects/"),
+        #("effects/layer", "effects/"),
+        ("effects/noValue", "effects/effect-no-value"),
+        ("effects/point", "effects/effect-value-point"),
+        ("effects/proLevels", "effects/pro-levels-effect"),
+        ("effects/slider", "effects/effect-value-slider"),
+        ("effects/stroke", "effects/stroke-effect"),
+        ("effects/tint", "effects/tint-effect"),
+        ("effects/tritone", "effects/tritone-effect"),
+        ("helpers/blendMode", "constants/blend-mode"),
+        ("helpers/boolean", "types/int-boolean"),
+        ("helpers/composite", "constants/composite"),
+        ("helpers/lineCap", "constants/line-cap"),
+        ("helpers/lineJoin", "constants/line-join"),
+        ("helpers/mask", "helpers/mask"),
+        ("helpers/textBased", "constants/text-based"),
+        #("helpers/textCaps", "constants/text-caps"),
+        ("helpers/textGrouping", "constants/text-grouping"),
+        ("helpers/textJustification", "constants/text-justify"),
+        ("helpers/textShape", "constants/text-shape"),
+        ("helpers/transform", "helpers/transform"),
+        ("layers/image", "layers/image-layer"),
+        ("layers/null", "layers/null-layer"),
+        ("layers/preComp", "layers/precomposition-layer"),
+        ("layers/shape", "layers/shape-layer"),
+        ("layers/solid", "layers/solid-color-layer"),
+        ("layers/text", "layers/text-layer"),
+        #("properties/doubleKeyframe", "animated-properties/"),
+        ("properties/gradient", "animated-properties/gradient-colors"),
+        ("properties/multiDimensional", "animated-properties/multi-dimensional"),
+        ("properties/multiDimensionalKeyframed", "animated-properties/multi-dimensional"),
+        #("properties/multiDimensionalSpatialKeyframed", "animated-properties/"),
+        ("properties/offsetKeyframe", "animated-properties/keyframe"),
+        #("properties/offsetSpatialKeyframe", "animated-properties/"),
+        ("properties/shape", "animated-properties/shape-property"),
+        ("properties/shapeKeyframed", "animated-properties/shape-property"),
+        ("properties/shapeProp", "helpers/bezier"),
+        ("properties/shapePropKeyframe", "animated-properties/shape-keyframe"),
+        ("properties/value", "animated-properties/value"),
+        #("properties/valueKeyframe", "animated-properties/keyframe"),
+        ("properties/valueKeyframed", "animated-properties/value"),
+        ("shapes/ellipse", "shapes/ellipse"),
+        ("shapes/fill", "shapes/fill"),
+        ("shapes/gFill", "shapes/gradient-fill"),
+        ("shapes/gStroke", "shapes/gradient-stroke"),
+        ("shapes/merge", "shapes/merge"),
+        ("shapes/rect", "shapes/rectangle"),
+        ("shapes/repeater", "shapes/repeater"),
+        ("shapes/round", "shapes/rounded-corners"),
+        ("shapes/shape", "shapes/path"),
+        ("shapes/star", "shapes/star"),
+        ("shapes/stroke", "shapes/stroke"),
+        ("shapes/transform", "shapes/transform"),
+        ("shapes/trim", "shapes/trim"),
+        ("sources/image", "assets/image"),
+        ("sources/precomp", "assets/precomposition"),
+        ("sources/chars", "text/character-data"),
+    ]
+
+    here = SchemaLocationPath(pathlib.Path(__file__).parent.parent / "docs" / "schema")
+    there = SchemaLocationPath(ns.schema_root)
+    here_unused = set(str(x.relative_to(here.root))[:-5] for x in here.root.glob("**/*.json"))
+    there_unused = set(str(x.relative_to(there.root))[:-5] for x in there.root.glob("**/*.json"))
+
+    for there_file, here_file in mappings:
+        here_unused.discard(here_file)
+        there_unused.discard(there_file)
+        summary_here = here.get_summary(here.fetch(here_file))
+        summary_there = there.get_summary(there.fetch(there_file))
+        prop_diff = summary_here.properties != summary_there.properties
+        req_diff = summary_here.required != summary_there.required
+
+        if not prop_diff and not req_diff:
+            continue
+
+        print("%s %s" % (here_file, there_file))
+        if prop_diff:
+            print("properties:")
+            diff_sets(summary_here.properties, summary_there.properties, False)
+
+        if req_diff:
+            print("required:")
+            diff_sets(summary_here.required, summary_there.required, False)
+
+        print("")
+
+    print("Files")
+    diff_sets(here_unused, there_unused, False)
 
 
 if __name__ == "__main__":
@@ -120,6 +228,10 @@ if __name__ == "__main__":
     diff.add_argument("object_ref_1")
     diff.add_argument("root_2")
     diff.add_argument("object_ref_2")
+
+    compare = sub.add_parser("compare-schemas", aliases=["c"])
+    compare.set_defaults(func=cmd_compare)
+    compare.add_argument("schema_root")
 
     ns = parser.parse_args()
     ns.func(ns)
