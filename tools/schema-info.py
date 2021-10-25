c5960a8e6f252e9a4b7b305be1a7f0df5a6f397e
Mattia Basaglia <glax@dragon.best>
2021-10-25 21:10:12 +0200

    Better schema mapping

--- tools/schema-info.py
+++ tools/schema-info.py
@@ -182,12 +182,36 @@ def cmd_compare(ns):
         ("sources/chars", "text/character-data"),
 
         (("layers/text", "properties", "t", "properties", "d", "properties", "k", "items", "properties", "s"), "text/text-document"),
+        (("layers/text", "properties", "t", "properties", "d", "properties", "k", "items"), "text/text-data-keyframe"),
+        (("layers/text", "properties", "t", "properties", "d"), "text/text-data"),
+        (("layers/text", "properties", "t"), "text/text-animator-data"),
     ]
+    here_ignored = {
+        "effects/effect-value",
+        "shapes/gradient",
+        "helpers/visual-object",
+        "layers/layer",
+        "shapes/modifier",
+        "shapes/shape-list",
+        "animated-properties/animated-property",
+        "text/font-list",
+        "animated-properties/color-value",
+        "shapes/shape",
+        "assets/asset",
+        "shapes/base-stroke",
+        "effects/effect",
+        "lottie.schema",
+        "shapes/shape-element",
+        "animation/composition",
+        "helpers/color",
+    }
 
     here = SchemaLocationPath(pathlib.Path(__file__).parent.parent / "docs" / "schema")
     there = SchemaLocationPath(ns.schema_root)
     here_unused = set(str(x.relative_to(here.root))[:-5] for x in here.root.glob("**/*.json"))
     there_unused = set(str(x.relative_to(there.root))[:-5] for x in there.root.glob("**/*.json"))
+
+    here_unused -= here_ignored
 
     for there_file, here_file in mappings:
         there_sub = tuple()
